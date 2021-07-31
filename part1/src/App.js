7d754e69f3f977b710886e5f72d536510e19ef4c
muqeet1 <muhammadmuqeet25@gmail.com>
2021-07-31 16:59:24 +0100

    use array.map to render contents

--- part1/src/App.js
+++ part1/src/App.js
@@ -19,41 +19,33 @@ const App = () => {
       },
     ]
   }
-
+  function Renderparts(thisaprop) {
+    const allparts = thisaprop.allparts.map(part => (
+      <p>{part.name + " "}
+        {part.exercises}
+      </p>
+    ))
+    return (<div>
+      {allparts}
+    </div>
+    )
+  }
 
   const Header = props => {
     return <h1>{props.course}</h1>
   }
 
-  const Part = props => {
-    return <p>{props.part.name} {props.part.exercises}</p>
-  }
-
-  const Content = props => {
-    return (
-      <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-      </div>
-    )
-  }
-
   const Total = props => {
-    return (
-      <p>Number of exercises {
-        props.numexe[0].exercises + props.numexe[1].exercises + props.numexe[2].exercises
-      }
-      </p>
-    )
+    const Numberexercise = props.numberexercises[0].exercises
+      + props.numberexercises[1].exercises
+      + props.numberexercises[2].exercises;
+    return <p>Total exercises are {Numberexercise}</p>;
   }
-
   return (
     <div>
       <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Part part={course.parts} />
-      <Total numexe={course.parts} />
+      <Renderparts allparts={course.parts} />
+      <Total numberexercises={course.parts} />
     </div>
   )
 
