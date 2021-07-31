2c61e5c147640d849e670fcbc216ac0457d6540e
muqeet1 <muhammadmuqeet25@gmail.com>
2021-07-31 18:41:43 +0100

    1.3 refactor code and app working

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,51 +2,46 @@ import React from "react";
 
 const App = () => {
 
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-    ]
+  const course = "Half Stack application development"
+  const part1 =
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
-  function Renderparts(thisaprop) {
-    const allparts = thisaprop.allparts.map(part => (
-      <p>{part.name + " "}
-        {part.exercises}
-      </p>
-    ))
-    return (<div>
-      {allparts}
-    </div>
-    )
+  const part2 =
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 =
+  {
+    name: 'State of a component',
+    exercises: 14
   }
 
   const Header = props => {
     return <h1>{props.course}</h1>
   }
+  const Part1 = props => {
+    return <p>{props.parts.name + " " + props.parts.exercises}</p>
+  }
+  const Part2 = props => {
+    return <p>{props.parts.name + " " + props.parts.exercises}</p>
+  }
+  const Part3 = props => {
+    return <p>{props.parts.name + " " + props.parts.exercises}</p>
+  }
 
   const Total = props => {
-    const Numberexercise = props.numberexercises.map(x => x.exercises)
-      .reduce((a, b) => (a + b)
-      )
-
-    return <p>Total exercises are {Numberexercise}</p>;
+    return <p>Total number of exercises are {props.numexe}</p>
   }
   return (
     <div>
-      <Header course={course.name} />
-      <Renderparts allparts={course.parts} />
-      <Total numberexercises={course.parts} />
+      <Header course={course} />
+      <Part1 parts={part1} />
+      <Part2 parts={part2} />
+      <Part3 parts={part3} />
+      <Total numexe={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 
