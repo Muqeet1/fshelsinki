613e9912eb0807fcd676b5d9dd7ffec137a5f97b
muqeet1 <muhammadmuqeet25@gmail.com>
2021-07-31 15:58:48 +0100

    exercise 1.1 - 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,38 +1,61 @@
 import React from "react";
 
-const App = () =>{
-const now = new Date();
-const a = 10;
-const b = 20;
-const Greeting = () => {
+const App = () => {
+
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  }
+
+
+  const Header = props => {
+    return <h1>{props.course}</h1>
+  }
+
+  const Part = props => {
+    return <p>{props.part.name} {props.part.exercises}</p>
+  }
+
+  const Content = props => {
+    return (
+      <div>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
+      </div>
+    )
+  }
+
+  const Total = props => {
+    return (
+      <p>Number of exercises {
+        props.numexe[0].exercises + props.numexe[1].exercises + props.numexe[2].exercises
+      }
+      </p>
+    )
+  }
+
   return (
     <div>
-      <p>render this tagger multiple times, well its a reusable component</p>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Part part={course.parts} />
+      <Total numexe={course.parts} />
     </div>
   )
-}
-const Passprops = props =>{
-  return <p>Hello, {props.name}</p>
-}
 
- return (
-  <div>
-    <p>Hello World! it is {now.toString()} </p>
-    <p>{a} plus {b} is {a+b} </p>
-   <div style={{textAlign: "center"}}> 
-    
-    <Greeting />
-    <Greeting />
-
-    <Passprops name="Sara" />
-    <Passprops name="Conner" />
-
-    <Greeting />
-    <Greeting />
-
-  </div>
-    
-  </div>
-  );
 }
 export default App;
