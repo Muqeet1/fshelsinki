89509943c7fa970b3db9105d81a00317a49ea8d6
muqeet1 <muhammadmuqeet25@gmail.com>
2021-10-23 00:24:48 +0100

    update part4: end to end testing with part5

--- part4/.eslintrc.js
+++ part4/.eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
-    jest: true
+    jest: true,
+    "cypress/globals": true
   },
   extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: 13,
   },
+  plugins: [ "react", "jest", "cypress" ],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
--- part4/app.js
+++ part4/app.js
@@ -1,35 +1,40 @@
-const config = require("./utils/config")
-const express = require("express")
-const loginRouter = require("./controllers/login")
-require("express-async-errors")
-const app = express()
-const logger = require("./utils/logger")
-const blogsRouter = require("./controllers/blogs")
-const usersRouter = require("./controllers/users")
-const middleware = require("./utils/middleware")
+const config = require("./utils/config");
+const express = require("express");
+const loginRouter = require("./controllers/login");
+require("express-async-errors");
+const app = express();
+const logger = require("./utils/logger");
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const middleware = require("./utils/middleware");
 //const { v4: uuidv4 } = require("uuid")
-const cors = require("cors")
-const mongoose = require("mongoose")
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-logger.info("connecting to", config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info("connected to MongoDB")
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.static("build"))
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 //app.use(middleware.userExtractor)
-app.use("/api/blogs", blogsRouter)
-app.use("/api/users", usersRouter)
-app.use("/api/login", loginRouter)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-module.exports = app
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+module.exports = app;
